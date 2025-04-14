60f0737172b09a07ac2650090067e69ec27156ce
PARKPARKWOO <wy9295@naver.com>
2025-04-14 22:15:04 +0900

    axios 요청시 withCredentials true

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -3,8 +3,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   baseURL: process.env.NODE_ENV === 'development'
     ? 'http://localhost:8080/api/v1'
-    : 'https://forest.platformholder.site/api/v1',
-    withCredentials: true,
+    : 'https://forest.platformholder.site/api/v1'
 });
 
 // 요청 인터셉터 추가
