3335fdf8d80e296372faa863d650c31416dbc5d4
PARKPARKWOO <wy9295@naver.com>
2025-10-12 22:01:08 +0900

    오탈자 수정

--- src/components/program/ProgramApplyDetailModal.jsx
+++ src/components/program/ProgramApplyDetailModal.jsx
@@ -156,8 +156,6 @@ export default function ProgramApplyDetailModal({ apply, programId, onClose }) {
                 })}
               </div>
             </div>
-          )}
-
           ) : (
             <div className="bg-yellow-50 border border-yellow-200 rounded-lg p-4">
               <div className="flex items-center">
